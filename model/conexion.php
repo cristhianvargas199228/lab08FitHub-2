8d82dfa2f8aaec0b17cef00e3edc740660ce3740
cristhianvargas199228 <94379795+cristhianvargas199228@users.noreply.github.com>
2023-05-08 17:14:52 -0500

    Add files via upload
    ya

---
+++ model/conexion.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'containers-us-west-150.railway.app';
+$dbname = 'railway';
+$username = 'root';
+$password = 'containers-us-west-150.railway.app';
+
+try {
+  $bd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+  $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Error al conectarse a la base de datos: " . $e->getMessage();
+  die();
+}
+?>
